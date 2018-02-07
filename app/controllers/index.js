ce354b448e548bb8d6f4734ba1c4a27f3ad706e3
Philip Laxamana <philip.laxamana@hotmail.com>
2018-02-06 19:25:49 -0500

    added jquery and bootstrap

---
+++ app/controllers/index.js
@@ -0,0 +1,7 @@
+//require
+
+
+//express
+
+
+//module.exports needed which points to a series of functions
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,8 @@
 let express = require('express');
 let router = express.Router();
 
+//need a require statement
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
